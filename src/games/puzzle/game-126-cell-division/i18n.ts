58c8af98938cc3c71a920d491ddfc293c45264c8
stephen.chuang <stephen.chuang@twsit.com>
2025-12-13 13:40:43 +0800

    feat: implement game 126 (cell-division)

---
+++ src/games/puzzle/game-126-cell-division/i18n.ts
@@ -0,0 +1,44 @@
+export const translations = {
+  'zh-TW': {
+    'game.title': '細胞分裂',
+    'game.desc': '透過細胞分裂佔領目標數量的格子！',
+    'game.start': '開始遊戲',
+    'game.reset': '重置',
+    'game.nextLevel': '下一關',
+    'game.level': '關卡',
+    'game.moves': '步數',
+    'game.cells': '細胞',
+    'game.target': '目標',
+    'game.win': '成功！',
+    'game.lose': '失敗！',
+    'game.complete': '恭喜完成所有關卡！'
+  },
+  'en': {
+    'game.title': 'Cell Division',
+    'game.desc': 'Divide cells to occupy the target number of spaces!',
+    'game.start': 'Start Game',
+    'game.reset': 'Reset',
+    'game.nextLevel': 'Next Level',
+    'game.level': 'Level',
+    'game.moves': 'Moves',
+    'game.cells': 'Cells',
+    'game.target': 'Target',
+    'game.win': 'Success!',
+    'game.lose': 'Failed!',
+    'game.complete': 'Congratulations! All levels completed!'
+  },
+  'ja': {
+    'game.title': '細胞分裂',
+    'game.desc': '細胞を分裂させて目標数のマスを占領しよう！',
+    'game.start': 'スタート',
+    'game.reset': 'リセット',
+    'game.nextLevel': '次のレベル',
+    'game.level': 'レベル',
+    'game.moves': '手数',
+    'game.cells': '細胞',
+    'game.target': '目標',
+    'game.win': '成功！',
+    'game.lose': '失敗！',
+    'game.complete': 'おめでとう！全レベルクリア！'
+  }
+};
