085d853e4ec27f7f2d4916368ad3b2b2f7731ba6
stephen.taipei <kingkongbboy@gmail.com>
2025-12-13 13:40:44 +0800

    feat: implement game 131 (telescope)

---
+++ src/games/puzzle/game-131-telescope/i18n.ts
@@ -0,0 +1,38 @@
+export const translations = {
+  'zh-TW': {
+    'game.title': '望遠鏡',
+    'game.desc': '移動望遠鏡找出所有星星！',
+    'game.start': '開始遊戲',
+    'game.reset': '重置',
+    'game.nextLevel': '下一關',
+    'game.level': '關卡',
+    'game.stars': '星星',
+    'game.win': '成功！',
+    'game.lose': '失敗！',
+    'game.complete': '恭喜完成所有關卡！'
+  },
+  'en': {
+    'game.title': 'Telescope',
+    'game.desc': 'Move the telescope to find all the stars!',
+    'game.start': 'Start Game',
+    'game.reset': 'Reset',
+    'game.nextLevel': 'Next Level',
+    'game.level': 'Level',
+    'game.stars': 'Stars',
+    'game.win': 'Success!',
+    'game.lose': 'Failed!',
+    'game.complete': 'Congratulations! All levels completed!'
+  },
+  'ja': {
+    'game.title': '望遠鏡',
+    'game.desc': '望遠鏡を動かして全ての星を見つけよう！',
+    'game.start': 'スタート',
+    'game.reset': 'リセット',
+    'game.nextLevel': '次のレベル',
+    'game.level': 'レベル',
+    'game.stars': '星',
+    'game.win': '成功！',
+    'game.lose': '失敗！',
+    'game.complete': 'おめでとう！全レベルクリア！'
+  }
+};
