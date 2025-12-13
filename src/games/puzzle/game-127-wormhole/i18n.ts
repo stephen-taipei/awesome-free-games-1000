04ec88160fd609a42971f0816bb9dca56a119482
stephen.chuang <stephen.chuang@twsit.com>
2025-12-13 13:40:44 +0800

    feat: implement game 127 (wormhole)

---
+++ src/games/puzzle/game-127-wormhole/i18n.ts
@@ -0,0 +1,38 @@
+export const translations = {
+  'zh-TW': {
+    'game.title': '蟲洞穿越',
+    'game.desc': '利用蟲洞傳送到達目標！',
+    'game.start': '開始遊戲',
+    'game.reset': '重置',
+    'game.nextLevel': '下一關',
+    'game.level': '關卡',
+    'game.moves': '步數',
+    'game.win': '成功！',
+    'game.lose': '失敗！',
+    'game.complete': '恭喜完成所有關卡！'
+  },
+  'en': {
+    'game.title': 'Wormhole',
+    'game.desc': 'Use wormholes to teleport and reach the goal!',
+    'game.start': 'Start Game',
+    'game.reset': 'Reset',
+    'game.nextLevel': 'Next Level',
+    'game.level': 'Level',
+    'game.moves': 'Moves',
+    'game.win': 'Success!',
+    'game.lose': 'Failed!',
+    'game.complete': 'Congratulations! All levels completed!'
+  },
+  'ja': {
+    'game.title': 'ワームホール',
+    'game.desc': 'ワームホールを使ってゴールを目指そう！',
+    'game.start': 'スタート',
+    'game.reset': 'リセット',
+    'game.nextLevel': '次のレベル',
+    'game.level': 'レベル',
+    'game.moves': '手数',
+    'game.win': '成功！',
+    'game.lose': '失敗！',
+    'game.complete': 'おめでとう！全レベルクリア！'
+  }
+};
