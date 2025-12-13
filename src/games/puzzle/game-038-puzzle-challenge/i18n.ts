4d948e0c72798fc0dabe04853a3f9f23b2b7e158
stephen.taipei <kingkongbboy@gmail.com>
2025-12-13 13:40:37 +0800

    feat: implement game 038 (puzzle-challenge)

---
+++ src/games/puzzle/game-038-puzzle-challenge/i18n.ts
@@ -0,0 +1,41 @@
+export const translations = {
+  'zh-TW': {
+    'game.title': '拼圖挑戰',
+    'game.desc': '在時間內完成拼圖！',
+    'game.start': '開始遊戲',
+    'game.reset': '重置',
+    'game.nextLevel': '下一關',
+    'game.level': '關卡',
+    'game.time': '時間',
+    'game.pieces': '拼圖',
+    'game.win': '完成！',
+    'game.lose': '時間到！',
+    'game.complete': '恭喜完成所有關卡！'
+  },
+  'en': {
+    'game.title': 'Puzzle Challenge',
+    'game.desc': 'Complete the puzzle before time runs out!',
+    'game.start': 'Start Game',
+    'game.reset': 'Reset',
+    'game.nextLevel': 'Next Level',
+    'game.level': 'Level',
+    'game.time': 'Time',
+    'game.pieces': 'Pieces',
+    'game.win': 'Complete!',
+    'game.lose': 'Time Up!',
+    'game.complete': 'Congratulations! All levels completed!'
+  },
+  'ja': {
+    'game.title': 'パズルチャレンジ',
+    'game.desc': '時間内にパズルを完成させよう！',
+    'game.start': 'スタート',
+    'game.reset': 'リセット',
+    'game.nextLevel': '次のレベル',
+    'game.level': 'レベル',
+    'game.time': '時間',
+    'game.pieces': 'ピース',
+    'game.win': 'クリア！',
+    'game.lose': 'タイムアップ！',
+    'game.complete': 'おめでとう！全レベルクリア！'
+  }
+};
