e93d9fdf92fa02dce6afeb91adcf4dba9df22840
stephen.taipei <kingkongbboy@gmail.com>
2025-12-13 13:40:46 +0800

    feat: implement game 165 (ring-toss)

---
+++ src/games/arcade/game-165-ring-toss/i18n.ts
@@ -0,0 +1,38 @@
+export const translations = {
+  'zh-TW': {
+    'game.title': '套圈圈',
+    'game.desc': '拋出圈圈套住柱子得分！',
+    'game.start': '開始遊戲',
+    'game.reset': '重新開始',
+    'game.score': '分數',
+    'game.lives': '回合',
+    'game.level': '關卡',
+    'game.gameOver': '遊戲結束',
+    'game.victory': '勝利！',
+    'game.finalScore': '最終分數'
+  },
+  'en': {
+    'game.title': 'Ring Toss',
+    'game.desc': 'Throw rings to land on pegs and score!',
+    'game.start': 'Start Game',
+    'game.reset': 'Play Again',
+    'game.score': 'Score',
+    'game.lives': 'Rounds',
+    'game.level': 'Level',
+    'game.gameOver': 'Game Over',
+    'game.victory': 'Victory!',
+    'game.finalScore': 'Final Score'
+  },
+  'ja': {
+    'game.title': '輪投げ',
+    'game.desc': '輪を投げてポールに入れよう！',
+    'game.start': 'スタート',
+    'game.reset': 'もう一度',
+    'game.score': 'スコア',
+    'game.lives': 'ラウンド',
+    'game.level': 'レベル',
+    'game.gameOver': 'ゲームオーバー',
+    'game.victory': '勝利！',
+    'game.finalScore': '最終スコア'
+  }
+};
